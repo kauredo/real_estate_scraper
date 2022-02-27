1984ad9985e40f96b3f0a7fed6ca4b48daa220ce
kauredo <vaskafig@gmail.com>
2022-02-27 02:29:51 +0000

    change carousel

--- app/javascript/components/homePage/Card.tsx
+++ app/javascript/components/homePage/Card.tsx
@@ -4,19 +4,19 @@ import { faEuroSign } from "@fortawesome/free-solid-svg-icons";
 
 export default function Card({ listing }) {
   return (
-    <div className="card max-w-sm min-h-max flex-shrink-0 relative w-full h-full shadow-md sm:w-auto">
-      {/* <div className="max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800"> */}
+    <div
+      style={{ marginTop: "-2rem" }}
+      className="card mx-auto max-w-sm flex-shrink-0 relative w-full h-full shadow-md sm:w-auto"
+    >
       <img
-        className="object-cover object-center w-full h-56"
+        className="object-cover object-center w-full h-2/3"
         src={listing.photos[0]}
         alt={listing.title}
       />
-
-      <div className="px-6 py-4 min-h-[10rem]">
+      <div className="px-6 py-4 h-1/3 justify-between flex flex-col">
         <h1 className="text-xl font-semibold text-gray-800 dark:text-white text-ellipsis">
-          {listing.title}
+          <TruncTitle title={listing.title} />
         </h1>
-
         <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
           <FontAwesomeIcon icon={faEuroSign} />
 
@@ -25,4 +25,12 @@ export default function Card({ listing }) {
       </div>
     </div>
   );
+}
+
+function TruncTitle({ title }) {
+  if (title.length > 90) {
+    return <span>{title.substring(0, 90) + "..."}</span>;
+  } else {
+    return <span>{title}</span>;
+  }
 }
--- app/javascript/components/homePage/Cards.tsx
+++ app/javascript/components/homePage/Cards.tsx
@@ -1,85 +1,35 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Card from "./Card";
+import Carousel from "nuka-carousel";
 
 export default function Cards({ listings }) {
-  const sliderRef = useRef(null);
-  const card = document.querySelector(".card");
-  let defaultTransform = 0;
-  console.log(card);
+  const [slideNumber, setSlideNumber] = useState(1);
+  useEffect(() => {
+    window.addEventListener("resize", handleResize, false);
+    handleResize();
+  }, []);
 
-  function goNext() {
-    defaultTransform = defaultTransform - 398;
-    const slider = sliderRef.current;
-    if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7)
-      defaultTransform = 0;
-    slider.style.transform = "translateX(" + defaultTransform + "px)";
-  }
-
-  function goPrev() {
-    const slider = sliderRef.current;
-    if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
-    else defaultTransform = defaultTransform + 398;
-    slider.style.transform = "translateX(" + defaultTransform + "px)";
-  }
-
+  const handleResize = () => {
+    const windowWidth = Math.floor(window.innerWidth / 412);
+    if (windowWidth == 0) {
+      setSlideNumber(1);
+    } else {
+      setSlideNumber(windowWidth);
+    }
+  };
   return (
     <div className="flex items-center justify-center w-full h-full py-8 px-4">
       {/* <!--- more free and premium Tailwind CSS components at https://tailwinduikit.com/ ---> */}
       <div className="w-full relative flex items-center justify-center">
-        <button
-          aria-label="slide backward"
-          className="absolute z-30 left-0 ml-10 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 cursor-pointer"
-          onClick={goPrev}
-        >
-          <svg
-            className="dark:text-gray-900"
-            width="8"
-            height="14"
-            viewBox="0 0 8 14"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M7 1L1 7L7 13"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </button>
         <div className="w-full h-full mx-auto overflow-y-hidden">
-          <div
-            ref={sliderRef}
-            className="h-full flex lg:gap-8 md:gap-6 gap-14 items-center justify-start transition ease-out duration-700"
-          >
-            {listings.map(listing => (
-              <Card listing={listing} key={listing.id} />
-            ))}
+          <div className="h-full flex lg:gap-8 md:gap-6 gap-14 items-center justify-start transition ease-out duration-700 my-2">
+            <Carousel heightMode="max" slidesToShow={slideNumber}>
+              {listings.map(listing => (
+                <Card listing={listing} key={listing.id} />
+              ))}
+            </Carousel>
           </div>
         </div>
-        <button
-          aria-label="slide forward"
-          className="absolute z-30 right-0 mr-10 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400"
-          onClick={goNext}
-        >
-          <svg
-            className="dark:text-gray-900"
-            width="8"
-            height="14"
-            viewBox="0 0 8 14"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1 1L7 7L1 13"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </button>
       </div>
     </div>
   );
