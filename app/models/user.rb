c1eeb34b5df1c05a938ef4bafcfc7684d76bc090
kauredo <vaskafig@gmail.com>
2022-03-06 22:19:50 +0000

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors.add attribute, (options[:message] || "não é um email válido")
+    end
+  end
+end
+
+class User < ApplicationRecord
+  validates :first_name, presence: { message: "não pode estar vazio" }
+  validates :last_name, presence: { message: "não pode estar vazio" }
+  validates :email, presence: { message: "não pode estar vazio" }, uniqueness: { case_sensitive: false }, email: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_27_030748) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_06_221058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_27_030748) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
