d336c698f7d4e045c87579c50998c8e9a0f1cf03
kauredo <vaskafig@gmail.com>
2022-03-22 20:34:12 +0000

    add top 25

--- app/javascript/components/homePage/Results.tsx
+++ app/javascript/components/homePage/Results.tsx
@@ -1,7 +1,11 @@
 import React, { useRef } from "react";
-import CountUp, { useCountUp } from "react-countup";
+import CountUp from "react-countup";
 import VisibilitySensor from "react-visibility-sensor";
 import { ResultNumbers } from "../utils/Interfaces";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouseChimney } from "@fortawesome/free-solid-svg-icons";
+import { faHandHoldingDollar } from "@fortawesome/free-solid-svg-icons";
+import { faMedal } from "@fortawesome/free-solid-svg-icons";
 
 interface Props {
   results: ResultNumbers;
@@ -13,11 +17,24 @@ export default function Results(props: Props) {
   return (
     <section className="container mx-auto">
       <div className="text-center w-full md:w-2/3 mx-auto py-20 text-2xl flex flex-col sm:flex-row justify-center align-center">
-        <div className="count mx-auto my-8 w-1/2">
+        <div className="count mx-auto my-8 w-1/3 flex flex-col">
+          <FontAwesomeIcon className="h-1/4 m-2 text-bordeaux" icon={faMedal} />
+          Nacional
+          <div>Top 25</div>
+        </div>
+        <div className="count mx-auto my-8 w-1/3 flex flex-col">
+          <FontAwesomeIcon
+            className="h-1/4 m-2 text-bordeaux"
+            icon={faHouseChimney}
+          />
           <div>Angariações</div>
           {listingCount}
         </div>
-        <div className="volume mx-auto my-8 w-1/2">
+        <div className="volume mx-auto my-8 w-1/3 flex flex-col">
+          <FontAwesomeIcon
+            className="h-1/4 m-2 text-bordeaux"
+            icon={faHandHoldingDollar}
+          />
           <div>Volume de Negócios</div>
           <CountUp end={volume} redraw={true} suffix=" €">
             {({ countUpRef, start }) => (
