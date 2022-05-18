004ff6507de81a7b528aaf165d1b0dc5eb73771d
kauredo <vaskafig@gmail.com>
2022-05-18 18:51:14 +0100

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   end
 
   devise_for :admins
-  resources :listings, only: [:show]
-  resources :listing_complexes, only: [:show]
+  # resources :listings, only: [:show]
+  # resources :listing_complexes, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -16,8 +16,10 @@ Rails.application.routes.draw do
   get "/casa_360", to: "pages#house_360", as: :house_360
   get "/contactos", to: "pages#contact", as: :contact
   post "/novo_contacto", to: "pages#new_contact", as: :new_contact
-  get "/novos", to: "listing_complexes#index", as: :latest
+  get "/empreendimentos", to: "listing_complexes#index", as: :latest
+  get '/empreendimentos/:id', to: 'listing_complexes#show', as: :listing_complex
   get "/comprar", to: "listings#buy", as: :buy
+  get '/comprar/:id', to: 'listings#show', as: :listing
   get "/vender", to: "listings#sell", as: :sell
   resources :newsletter_subscriptions, only: [:create, :destroy] do
     member do
