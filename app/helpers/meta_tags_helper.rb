41ad2ddbcc27dc7136cf9cf88d64d235839e875b
kauredo <vaskafig@gmail.com>
2023-01-13 15:08:49 +0000

    fix meta tags for localization

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -3,19 +3,29 @@
 module MetaTagsHelper
   def meta_title
     if request.path_info.include?('backoffice')
-      DEFAULT_META['meta_title_backoffice']
+      localized_meta['meta_title_backoffice']
     else
-      content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META['meta_title']
+      content_for?(:meta_title) ? content_for(:meta_title) : localized_meta['meta_title']
     end
   end
 
   def meta_description
-    content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META['meta_description']
+    content_for?(:meta_description) ? content_for(:meta_description) : localized_meta['meta_description']
   end
 
   def meta_image
-    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META['meta_image'])
+    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : localized_meta['meta_image'])
     # little twist to make it work equally with an asset or a url
     meta_image.starts_with?('http') ? meta_image : image_url("/images/#{meta_image}")
+  end
+
+  private
+
+  def localized_meta
+    if I18n.locale == I18n.default_locale
+      DEFAULT_META
+    else
+      DEFAULT_META_EN
+    end
   end
 end
