fa3e1afbeff483f9c88e47f909f08f70d1213299
kauredo <vaskafig@gmail.com>
2023-03-30 10:39:44 +0100

    remove sentry locally

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,7 @@
 Sentry.init do |config|
   config.dsn = ENV['SENTRY_URL']
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
+  config.enabled_environments = %w[production staging]
 
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
