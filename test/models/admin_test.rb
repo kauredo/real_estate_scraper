b474febaf5e12c04b72221f3c113b0c09095cca6
kauredo <vaskafig@gmail.com>
2023-04-09 21:30:29 +0100

    add unit tests to admis

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -3,7 +3,28 @@
 require 'test_helper'
 
 class AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @admin = Admin.new(email: 'test@admin.com', password: 'password')
+  end
+
+  test 'should be valid' do
+    assert @admin.valid?
+  end
+
+  test 'email should be present' do
+    @admin.email = ' '
+    assert_not @admin.valid?
+  end
+
+  test 'password should be present' do
+    @admin.password = ' '
+    assert_not @admin.valid?
+  end
+
+  test 'email should be unique' do
+    duplicate_admin = @admin.dup
+    duplicate_admin.email = @admin.email.upcase
+    @admin.save
+    assert_not duplicate_admin.valid?
+  end
 end
