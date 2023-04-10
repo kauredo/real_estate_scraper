6de70e67a644eb4cf1d2b1ed69d39d476baef95d
kauredo <vaskafig@gmail.com>
2023-04-10 14:21:49 +0100

    add unit tests to backoffice controller

---
+++ test/controllers/backoffice_controller_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class BackofficeControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @admin = admins(:one)
+    @admin.update(confirmed: true)
+    sign_in @admin
+  end
+
+  test 'should get index when admin is authenticated and confirmed' do
+    get backoffice_url
+    assert_response :success
+  end
+
+  test 'should redirect to root path when admin is not confirmed' do
+    @admin.update(confirmed: nil)
+    get backoffice_url
+    assert_redirected_to root_path
+  end
+
+  test 'should redirect to root path when admin is not authenticated' do
+    sign_out @admin
+    get backoffice_url
+    assert_redirected_to new_admin_session_path
+  end
+end
