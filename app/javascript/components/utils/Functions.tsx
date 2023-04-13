30573c4ffa377470fccef9a1343310833d1cab5c
kauredo <vaskafig@gmail.com>
2023-04-13 14:46:01 +0100

    fix address error

--- app/javascript/components/utils/Functions.tsx
+++ app/javascript/components/utils/Functions.tsx
@@ -1,7 +1,7 @@
 import { i18n } from "../../languages/languages";
 
 export function truncateText(title, length) {
-  if (title.length > 90) {
+  if (title && title.length > 90) {
     return title.substring(0, length) + "...";
   } else {
     return title;
