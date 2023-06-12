e2744e47c2563f9a8423b0067e5ce2847f87460a
kauredo <vaskafig@gmail.com>
2023-06-12 19:49:26 +0100

    fix scheduler

--- config/initializers/sidekiq_scheduler.rb
+++ config/initializers/sidekiq_scheduler.rb
@@ -5,6 +5,8 @@ require 'sidekiq-scheduler'
 
 Sidekiq.configure_server do |config|
   config.on(:startup) do
+    return if Rails.env.staging?
+
     Sidekiq.schedule = YAML.load_file(File.expand_path('../sidekiq_scheduler.yml', __dir__))
     SidekiqScheduler::Scheduler.instance.reload_schedule!
   end
