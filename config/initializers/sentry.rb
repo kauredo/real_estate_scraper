22135e115fcfe07a02dad5ff307eeb2c1f79d3d5
kauredo <vaskafig@gmail.com>
2023-06-19 16:43:47 +0100

    tinymce fix

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,7 +3,7 @@
 Sentry.init do |config|
   config.dsn = ENV['SENTRY_URL']
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
-  config.enabled_environments = %w[production staging]
+  config.enabled_environments = %w[production]
 
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
