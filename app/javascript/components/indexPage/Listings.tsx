32a689dfb28b922fdc4220c43d03b39390268e2c
kauredo <vaskafig@gmail.com>
2024-10-03 18:45:01 +0100

    working status filter

--- app/javascript/components/indexPage/Listings.tsx
+++ app/javascript/components/indexPage/Listings.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Listing } from "../utils/Interfaces";
 import LongCard from "./LongCard";
+import { i18n } from "../../languages/languages";
+import { sanitizeURL } from "../utils/Functions";
 
 interface Props {
   listings: Listing[];
@@ -9,6 +11,27 @@ interface Props {
 
 export default function Listings(props: Props) {
   const { listings, backoffice } = props;
+
+  if (listings.length === 0) {
+    console.log("No listings");
+    return (
+      <div>
+        <div className="container mx-auto">
+          <div className="flex flex-col justify-center items-center pt-4">
+            <p className="text-xl text-gray-500 text-center">
+              {i18n.t("listing.no_listings")}
+            </p>
+            <a
+              href={sanitizeURL(window.Routes.buy_path)}
+              className="text-beige text-xl font-bold underline ml-2"
+            >
+              {i18n.t("listing.reset_filters")}
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="container mx-auto">
--- app/javascript/components/shared/ListingSearch.tsx
+++ app/javascript/components/shared/ListingSearch.tsx
@@ -9,6 +9,7 @@ interface Props {
     address_cont?: string;
     price_gteq?: number;
     price_lteq?: number;
+    status_eq?: string;
   };
   listingMaxPrice: number;
 }
@@ -18,6 +19,7 @@ export default function PriceSlider(props: Props) {
 
   const [title, setTitle] = useState(params?.title_cont || "");
   const [address, setAddress] = useState(params?.address_cont || "");
+  const [status, setStatus] = useState(params?.status_eq || "");
   const [prices, setPrices] = useState([
     params?.price_gteq || 0,
     params?.price_lteq || listingMaxPrice,
@@ -29,10 +31,34 @@ export default function PriceSlider(props: Props) {
     }
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    // Add price parameters to the form data
+    formData.append("q[price_gteq]", prices[0].toString());
+    formData.append("q[price_lteq]", prices[1].toString());
+
+    // Convert FormData to object
+    const formDataObject: Record<string, string> = {};
+    formData.forEach((value, key) => {
+      if (typeof value === "string") {
+        formDataObject[key] = value;
+      }
+    });
+
+    // Submit form
+    const action = e.currentTarget.action;
+    window.location.href = `${action}?${new URLSearchParams(formDataObject)}`;
+  };
+
   return (
     <div className="container mx-auto lg:px-0 sm:px-6 px-4">
       <h2 className="text-xl mb-4">{i18n.t("listing.search.title")}</h2>
-      <form action={sanitizeURL(window.Routes.buy_path)}>
+      <form
+        action={sanitizeURL(window.Routes.buy_path)}
+        onSubmit={handleSubmit}
+      >
         <div className="w-full flex flex-wrap justify-between align-center gap-2">
           <div className="mb-4 w-full md:w-[23%]">
             <label htmlFor="q_title_cont" className="block mb-1">
@@ -61,6 +87,34 @@ export default function PriceSlider(props: Props) {
             />
           </div>
           <div className="mb-4 w-full md:w-[23%]">
+            {/* status */}
+            <label htmlFor="q_status_eq" className="block mb-1">
+              {i18n.t("listing.search.status.title")}
+            </label>
+            <select
+              name="q[status_eq]"
+              id="q_status_eq"
+              className="w-full p-2 rounded-md border border-gray-300"
+              value={status}
+              onChange={e => setStatus(e.target.value)}
+            >
+              <option value="">{i18n.t("listing.search.status.all")}</option>
+              <option value="0">
+                {i18n.t("listing.search.status.recent")}
+              </option>
+              <option value="2">
+                {i18n.t("listing.search.status.agreed")}
+              </option>
+              <option value="3">{i18n.t("listing.search.status.sold")}</option>
+              <option value="4">
+                {i18n.t("listing.search.status.rented")}
+              </option>
+              <option value="5">
+                {i18n.t("listing.search.status.closed")}
+              </option>
+            </select>
+          </div>
+          <div className="mb-4 w-full md:w-[23%]">
             <label htmlFor="q_price_lteq" className="block mb-1">
               {i18n.t("listing.search.price")}
             </label>
@@ -69,10 +123,7 @@ export default function PriceSlider(props: Props) {
                 range
                 min={0}
                 max={listingMaxPrice}
-                defaultValue={[
-                  params?.price_gteq || 0,
-                  params?.price_lteq || listingMaxPrice,
-                ]}
+                defaultValue={[prices[0], prices[1]]}
                 onChange={handlePriceChange}
                 allowCross={false}
                 step={100}
