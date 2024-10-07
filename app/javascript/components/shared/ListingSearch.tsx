67c3288547a2b40c47faed3f66cdc48b09e38ac4
kauredo <vaskafig@gmail.com>
2024-10-07 13:49:02 +0100

    melhorar filtros

--- app/javascript/components/shared/ListingSearch.tsx
+++ app/javascript/components/shared/ListingSearch.tsx
@@ -137,8 +137,11 @@ export default function PriceSlider(props: Props) {
           </div>
           {otherStatsKeys.map(key => {
             return (
-              <div key={key} className="w-full md:w-[23%]">
-                <label htmlFor={`q_${key}`} className="block mb-1">
+              <div key={key} className="w-full md:w-[calc((23%/2)-0.75rem)]">
+                <label
+                  htmlFor={`q_${key}`}
+                  className="block mb-1 whitespace-nowrap"
+                >
                   {i18n.t(`listing.stats.${key.toLowerCase()}`)}
                 </label>
                 <select
