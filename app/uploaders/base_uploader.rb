79c9e7682d62928e3da1b79c33fb67b1fb533ddd
kauredo <vaskafig@gmail.com>
2025-03-31 09:16:31 +0100

    move cloudinary to baseuploader

--- app/uploaders/base_uploader.rb
+++ app/uploaders/base_uploader.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class BaseUploader < CarrierWave::Uploader::Base
+  include Cloudinary::CarrierWave if !Rails.env.test? && !ENV['USE_LOCAL_STORAGE']
+
   storage :file if Rails.env.test? || ENV['USE_LOCAL_STORAGE']
   storage :cloudinary unless Rails.env.test? || ENV['USE_LOCAL_STORAGE']
 
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class ImageUploader < BaseUploader
-  include Cloudinary::CarrierWave
 end
