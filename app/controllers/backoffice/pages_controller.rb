f4a1a3a420ca609b0c26a995de7e529af731933f
kauredo <vaskafig@gmail.com>
2025-04-04 15:47:56 +0100

    improve backoffice layouts

--- app/controllers/backoffice/pages_controller.rb
+++ app/controllers/backoffice/pages_controller.rb
@@ -5,6 +5,7 @@ module Backoffice
     def home
       @variables = Variable.all
       @subs = NewsletterSubscription.includes(:user).where(user: { confirmed_email: true })
+      @partners = Partner.all
     end
   end
 end
