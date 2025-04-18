b757b2a2158b67b032de2f946397d4982900fa81
kauredo <vaskafig@gmail.com>
2025-04-18 17:43:16 +0100

    modify flipper add comments

--- app/models/feature_flag.rb
+++ app/models/feature_flag.rb
@@ -1,3 +1,8 @@
-class FeatureFlag < ActiveRecord::Base
+# frozen_string_literal: true
+
+class FeatureFlag < ApplicationRecord
+  # Include Flipper::Model::Feature module to use Flipper for feature flags
+  # This allows you to use the Flipper gem to manage feature flags in your application
+  # For more information, visit
   include Flipper::Model::Feature
 end
--- app/models/feature_gate.rb
+++ app/models/feature_gate.rb
@@ -1,3 +1,8 @@
-class FeatureGate < ActiveRecord::Base
+# frozen_string_literal: true
+
+class FeatureGate < ApplicationRecord
+  # Include Flipper::Model::Gate to enable the feature gate functionality
+  # This allows you to use the Flipper gem to manage feature flags in your application
+  # For more information, visit
   include Flipper::Model::Gate
 end
