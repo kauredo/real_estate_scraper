a9324537a94cb46358e97dfea52787ff84e683c8
kauredo <vaskafig@gmail.com>
2025-04-18 18:31:47 +0100

    don't init flipper when precompiling assets

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -3,12 +3,14 @@
 require 'flipper'
 require 'flipper/adapters/active_record'
 
-Rails.application.config.after_initialize do
-  if ActiveRecord::Base.connection.table_exists?('flipper_features')
-    Flipper.configure do |config|
-      config.default do
-        adapter = Flipper::Adapters::ActiveRecord.new
-        Flipper.new(adapter)
+unless ENV['RAILS_GROUPS'] == 'assets'
+  Rails.application.config.after_initialize do
+    if ActiveRecord::Base.connection.table_exists?('flipper_features')
+      Flipper.configure do |config|
+        config.default do
+          adapter = Flipper::Adapters::ActiveRecord.new
+          Flipper.new(adapter)
+        end
       end
     end
   end
