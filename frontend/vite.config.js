9586f4d997dc6dfba32182fbe1cc6b4be049e50f
kauredo <vaskafig@gmail.com>
2025-04-25 11:33:59 +0100

    attempt moving assets

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
     proxy: {
       "/api": {
