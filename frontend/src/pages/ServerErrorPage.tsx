d59509560a31d98fcd58c3d8bca90dbfba4da4a2
kauredo <vaskafig@gmail.com>
2025-05-02 03:29:39 +0100

    privacy page

--- frontend/src/pages/ServerErrorPage.tsx
+++ frontend/src/pages/ServerErrorPage.tsx
@@ -1,13 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import ServerError from "../components/svgs/ServerError";
 import { useMetaTags } from "../hooks/useMetaTags";
 import mainWhiteLogo from "../assets/logos/main_white.webp";
 import mainLogo from "../assets/logos/main.webp";
-import NotFound from "../components/svgs/NotFound";
 
-const NotFoundPage = () => {
+const ServerErrorPage = () => {
   const { t } = useTranslation();
 
   useMetaTags({
@@ -36,27 +34,27 @@ const NotFoundPage = () => {
           </div>
           <div className="mb-10 md:mb-20 text-gray-600 dark:text-light font-light">
             <h1 className="font-black uppercase text-3xl lg:text-5xl text-beige-default dark:text-beige-medium mb-10">
-              {t("errors.404.title")}
+              {t("errors.500.title")}
             </h1>
-            <p>{t("errors.404.text_1")}</p>
-            <p>{t("errors.404.text_2")}</p>
+            <p>{t("errors.500.text_1")}</p>
+            <p>{t("errors.500.text_2")}</p>
           </div>
           <div className="mb-20 md:mb-0">
             <Link
               to="/"
               className="text-lg font-light outline-none focus:outline-none transform transition-all hover:scale-110 text-beige-default dark:text-beige-medium hover:brightness-50 dark:hover:text-light dark:hover:brightness-100"
             >
-              <FontAwesomeIcon icon="arrow-left" className="mr-2" />
-              {t("errors.404.button")}
+              <i className="mdi mdi-arrow-left mr-2"></i>
+              {t("errors.500.button")}
             </Link>
           </div>
         </div>
         <div className="w-full md:w-1/2 text-center">
-          <NotFound />
+          <ServerError />
         </div>
       </div>
     </div>
   );
 };
 
-export default NotFoundPage;
+export default ServerErrorPage;
