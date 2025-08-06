001153fc63cda3af689f4b160b0044108d3387ab
kauredo <vaskafig@gmail.com>
2025-08-06 14:41:24 +0100

    good job improvements

--- app/sidekiq/scrape_url_job.rb
+++ app/sidekiq/scrape_url_job.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'timeout'
+
 class ScrapeUrlJob < ApplicationJob
   queue_as :individual_scraping
   queue_with_priority 5
@@ -7,10 +9,15 @@ class ScrapeUrlJob < ApplicationJob
   retry_on(StandardError, wait: 10.minutes, attempts: 3)
   discard_on(Timeout::Error)
 
+  # Add job timeout at the GoodJob level
+  queue_as :individual_scraping, priority: 5, job_timeout: 20.minutes
+
   around_perform do |job, block|
-    block.call
+    Timeout.timeout(20.minutes) do
+      block.call
+    end
   rescue Timeout::Error => e
-    ScrapeListingDetails.log("[ScrapeUrlJob] Timed out after 5 minutes for #{job.arguments.first}")
+    ScrapeListingDetails.log("[ScrapeUrlJob] Timed out after 20 minutes for #{job.arguments.first}")
     raise e
   end
 
@@ -18,11 +25,17 @@ class ScrapeUrlJob < ApplicationJob
     start_time = Time.current
     ScrapeListingDetails.log("[ScrapeUrlJob] Starting #{url} at #{start_time}")
 
-    scraper_service = RealEstateScraperService.new
+    # Add timeout for browser creation
+    scraper_service = Timeout.timeout(2.minutes) do
+      RealEstateScraperService.new
+    end
     service_start = Time.current
     ScrapeListingDetails.log("[ScrapeUrlJob] Browser created in #{service_start - start_time} seconds")
 
-    scraper_service.scrape_one(url, nil, force:)
+    # Add timeout for the actual scraping
+    Timeout.timeout(15.minutes) do
+      scraper_service.scrape_one(url, nil, force:)
+    end
     scrape_end = Time.current
 
     total_time = scrape_end - start_time
